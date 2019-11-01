b4dba29099a5e017dacd918cce7ab2bc1ae188e6
Sam Jackson <samuelsjackson7@gmail.com>
2019-10-31 21:26:34 -0400

    codewars

--- loveVsFriendship.js
+++ loveVsFriendship.js
@@ -1,5 +1,15 @@
+// a=1, b=2 ... z=26
+
 const wordsToMarks = string => {
   const alph = 'abcdefghijklmnopqrstuvwxyz';
   let sum = 0;
-  string.split('');
+  string.split('').forEach(x => (sum += alph.indexOf(x) + 1));
+  return sum;
 };
+
+console.log(wordsToMarks('love'));
+
+// other answer
+
+const wordsToMarks = s =>
+  [...s].map((a, i) => s.charCodeAt(i) - 96).reduce((a, b) => a + b);

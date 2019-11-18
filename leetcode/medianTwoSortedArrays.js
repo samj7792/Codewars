82bb0e862a751098286325b25e930f43698aff06
Sam Jackson <samuelsjackson7@gmail.com>
2019-11-18 11:14:51 -0500

    leetcode

---
+++ leetcode/medianTwoSortedArrays.js
@@ -0,0 +1,10 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+  let full = nums1.concat(nums2);
+  full.sort((a, b) => a - b);
+  console.log(full);
+  if (full.length % 2 == 1) {
+    return full[Math.floor(full.length / 2)];
+  } else {
+    return (full[full.length / 2] + full[full.length / 2 - 1]) / 2;
+  }
+};

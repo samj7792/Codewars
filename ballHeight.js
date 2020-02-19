fd8d0de4d555d886484f45762b4065036f011062
Sam Jackson <samuelsjackson7@gmail.com>
2020-02-18 22:36:55 -0500

    codewars

--- ballHeight.js
+++ ballHeight.js
@@ -1,12 +1,17 @@
 const maxBall = v0 => {
   let v = (v0 * 1000) / 3600;
   let h = 0;
-  let g = 9.8;
+  let g = 9.81;
   const heights = [];
   for (let t = 0; h > -0.1; t += 0.1) {
     h = v * t - 0.5 * g * t * t;
-    heights.push(parseFloat(h.toFixed(2)));
+    heights.push(h);
   }
   const max = Math.max(...heights);
-  return heights.indexOf(max);
+  const index = heights.indexOf(max);
+  return index;
 };
+
+// from codewars
+
+const maxBall = v => Math.round(v / 3.5316);

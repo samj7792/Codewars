54cccfa05703c35a6dbc460743aa9bba5f524fdd
Sam Jackson <samuelsjackson7@gmail.com>
2020-09-24 21:25:58 -0400

    codewars

--- sumArraySingles.js
+++ sumArraySingles.js
@@ -2,21 +2,20 @@
 
 const arr = [1, 3, 1, 7, 3, 8];
 
-const nonDups = arr.filter((x) => {
-  if (arr.indexOf(x) == arr.lastIndexOf(x)) {
-    return x;
-  }
-});
+// const repeats = (arr) =>
+//   arr
+//     .filter((x) => {
+//       if (arr.indexOf(x) == arr.lastIndexOf(x)) {
+//         return x;
+//       }
+//     })
+//     .reduce((acc, curr) => acc + curr);
 
-const total = nonDups.reduce((acc, curr) => acc + curr);
+// sightly simplified
 
 const repeats = (arr) =>
   arr
-    .filter((x) => {
-      if (arr.indexOf(x) == arr.lastIndexOf(x)) {
-        return x;
-      }
-    })
+    .filter((x) => arr.indexOf(x) == arr.lastIndexOf(x))
     .reduce((acc, curr) => acc + curr);
 
 console.log(repeats(arr));

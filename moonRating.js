0eda6975f517355e709e7ae1f2227188261349c2
Sam Jackson <samuelsjackson7@gmail.com>
2020-09-27 01:17:49 -0400

    codewars

--- moonRating.js
+++ moonRating.js
@@ -3,35 +3,19 @@ const halfMoon = "c";
 const newMoon = "x";
 
 const moonRating = (num) => {
-  // const full = full = Math.floor(num);
-  // const half = Math.ceil((num % 1) / 5) * 5;
-  // const neww = Math.floor(10 - num);
-  const rating = [
-    (full = Math.floor(num)),
-    (half = Math.ceil((num % 1) / 5) * 5),
-    (neww = Math.floor(10 - num)),
-  ];
-  console.log(rating);
-
-  const string = [];
-  rating.map((x, j) => {
-    if (j == 0) {
-      for (let i = 0; i < x; i + )
-    }
-
-
-
-    for (let i = 0; i < x; i++) {
-      if (j == 0) {
-        string.push("o");
-      } else if (j == 1) {
-        string.push("c");
-      } else {
-        string.push("x");
-      }
-    }
-  });
-  console.log(string);
+  console.log(fullMoon.repeat(num / 2));
+  console.log(halfMoon.repeat(Math.round(num) % 2));
+  console.log(newMoon.repeat((10 - Math.ceil(num)) / 2));
+  return (
+    "o".repeat(Math.round(num) / 2) +
+    "c".repeat(Math.round(num) % 2) +
+    "x".repeat((10 - Math.ceil(num)) / 2)
+  );
 };
 
-console.log(moonRating(7.2));
+console.log(moonRating(6.1));
+
+// ^^^ not working on even numbers with decimal below .5
+
+moonRating = (x) =>
+  ("o".repeat((x += 0.5) >> 1) + ["c"[~x & 1]]).padEnd(5, "x");
